900b6ff5d1c7b12f66a7fb84b01e2a761c691b42
Lugo <bl1ndcod3@gmail.com>
2025-09-08 20:25:08 -0600

    feat: agregar columna isDeceased a la tabla de préstamos
    - Se añadió la columna `isDeceased` en la tabla `loans` para registrar el estado de deceso de los préstamos, permitiendo una mejor gestión de esta información en el sistema.

--- migrations/20250909011457_add_is_deceased_to_loan/migration.sql
+++ migrations/20250909011457_add_is_deceased_to_loan/migration.sql
@@ -1,17 +1,1 @@
--- DropIndex
-DROP INDEX "idx_account_type";
-
--- DropIndex
-DROP INDEX "idx_loan_lead_dates";
-
--- DropIndex
-DROP INDEX "idx_loanpayment_loan_date";
-
--- DropIndex
-DROP INDEX "idx_transaction_route_date";
-
--- DropIndex
-DROP INDEX "idx_transaction_source_account";
-
--- DropIndex
-DROP INDEX "idx_transaction_type_source";
+ALTER TABLE "loans" ADD COLUMN "isDeceased" BOOLEAN NOT NULL DEFAULT false;
