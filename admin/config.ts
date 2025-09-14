833f2992d12ed803f854380b7f7c6010dd61ef44
Lugo <bl1ndcod3@gmail.com>
2025-09-14 12:49:01 -0600

    feat: agregar funcionalidad para gestionar cartera muerta en el sistema
    - Se implementaron nuevas mutaciones y consultas GraphQL para marcar préstamos como cartera muerta y obtener un resumen de estos préstamos.
    - Se añadió una nueva página en la interfaz de administración para gestionar la cartera muerta, permitiendo a los usuarios seleccionar préstamos y marcarlos con una fecha específica.
    - Se mejoró la lógica de consulta para filtrar préstamos elegibles basándose en semanas desde el crédito y semanas sin pago, optimizando la gestión de deudas.
    - Se incorporaron estilos y componentes personalizados para mejorar la experiencia del usuario en la nueva funcionalidad.

--- admin/config.ts
+++ admin/config.ts
@@ -1,7 +1,9 @@
 // /admin/config.ts
 import type { AdminConfig } from '@keystone-6/core/types';
 import { CustomHeader } from './components/CustomHeader';
+import { CustomNavigation } from './components/CustomNavigation';
 
 export const components: AdminConfig['components'] = {
-  Logo: CustomHeader
+  Logo: CustomHeader,
+  Navigation: CustomNavigation
 };
